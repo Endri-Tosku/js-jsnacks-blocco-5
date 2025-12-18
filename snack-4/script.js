c2e7c6fbe38fae24f76e26c8f62185e81f16ac65
Endri-Tosku <endritosku001@gmail.com>
2025-12-18 14:54:56 +0100

    feat: finito snack 4

--- snack-4/script.js
+++ snack-4/script.js
@@ -7,3 +7,10 @@ const posts = [
 
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
+
+// Uso map() per creare un nuovo array contenente solo i nomi
+const onlyName = posts.map((post) => {
+  return post.author
+})// Trasformiamo ogni oggetto in una stringa con il nome della persona
+
+console.log(onlyName);
