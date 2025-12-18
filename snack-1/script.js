f805bb790e1d02dd612f1a6eb416fa9c5e8f5a52
Endri-Tosku <endritosku001@gmail.com>
2025-12-18 14:29:29 +0100

    feat: copletato il primo snack

--- snack-1/script.js
+++ snack-1/script.js
@@ -2,3 +2,15 @@ const names = ['Edoardo', 'Simone', 'Francesco'];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
+
+// Uso "map()" per creare un nuovo array in cui ogni nome è racchiuso tra virgolette singole
+const quotedNames = names.map(name => `'${name}'`); 
+
+// Uso join() per unire tutti gli elementi dell'array in una singola stringa separata da virgola e spazio
+const result = quotedNames.join(', ');
+
+console.log(result); 
+
+//// Versione compattata ////
+console.log(names.map(name => `'${name}'`).join(', '));
+
